f8f3d076ec713f1b7de7f99f32972c24eb453e99
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-02-10 19:39:19 +0100

    Add tests for transformations

--- tests/FastDictTest.elm
+++ tests/FastDictTest.elm
@@ -23,6 +23,7 @@ suite =
         , memberTest
         , getTest
         , sizeTest
+        , equalTest
 
         -- Lists
         , keysTest
@@ -32,13 +33,18 @@ suite =
 
         -- Transform
         , mapTest
-
-        -- Low level
-        , equalTest
+        , foldlTest
+        , foldrTest
+        , filterTest
+        , partitionTest
 
         -- elm/core
         , elmCoreTests
         ]
+
+
+
+-- Build --
 
 
 emptyTest : Test
@@ -214,6 +220,10 @@ removeTest =
         ]
 
 
+
+-- Query --
+
+
 isEmptyTest : Test
 isEmptyTest =
     describe "isEmpty"
@@ -289,6 +299,24 @@ sizeTest =
                 (Dict.size dict == 0)
                     |> Expect.equal (Dict.isEmpty dict)
         ]
+
+
+equalTest : Test
+equalTest =
+    describe "equal is not magic"
+        [ test "Different structure means you can't use ==" <|
+            \_ ->
+                veryBalanced 12
+                    |> Expect.notEqual (veryUnbalanced 12)
+        , fuzz2 dictFuzzer dictFuzzer "Is True iff equivalent via toList" <|
+            \left right ->
+                (left |> Dict.equals right)
+                    |> Expect.equal (Dict.toList left == Dict.toList right)
+        ]
+
+
+
+-- Lists --
 
 
 keysTest : Test
@@ -378,6 +406,10 @@ fromListTest =
         ]
 
 
+
+-- Transform --
+
+
 mapTest : Test
 mapTest =
     let
@@ -425,17 +457,94 @@ mapTest =
         )
 
 
-equalTest : Test
-equalTest =
-    describe "equal is not magic"
-        [ test "Different structure means you can't use ==" <|
-            \_ ->
-                veryBalanced 12
-                    |> Expect.notEqual (veryUnbalanced 12)
-        , fuzz2 dictFuzzer dictFuzzer "Is True iff equivalent via toList" <|
-            \left right ->
-                (left |> Dict.equals right)
-                    |> Expect.equal (Dict.toList left == Dict.toList right)
+foldlTest : Test
+foldlTest =
+    describe "foldl"
+        [ fuzz dictFuzzer "foldl (::) is equivalent to toList >> reverse" <|
+            \dict ->
+                dict
+                    |> Dict.foldl (\k v -> (::) ( k, v )) []
+                    |> Expect.equalLists (List.reverse <| Dict.toList dict)
+        , fuzz dictFuzzer "foldl insert is an identity" <|
+            \dict ->
+                dict
+                    |> Dict.foldl Dict.insert Dict.empty
+                    |> expectEqual dict
+        ]
+
+
+foldrTest : Test
+foldrTest =
+    describe "foldr"
+        [ fuzz dictFuzzer "foldr (::) is equivalent to toList" <|
+            \dict ->
+                dict
+                    |> Dict.foldr (\k v -> (::) ( k, v )) []
+                    |> Expect.equalLists (Dict.toList dict)
+        , fuzz dictFuzzer "foldr insert is an identity" <|
+            \dict ->
+                dict
+                    |> Dict.foldr Dict.insert Dict.empty
+                    |> expectEqual dict
+        ]
+
+
+filterTest : Test
+filterTest =
+    let
+        f : Key -> Value -> Bool
+        f _ v =
+            modBy 2 v == 0
+
+        filteredFuzzer =
+            Fuzz.map (Dict.filter f) dictFuzzer
+    in
+    describe "filter"
+        [ fuzz dictFuzzer "Is equivalent to toList >> List.filter >> fromList" <|
+            \dict ->
+                dict
+                    |> Dict.filter f
+                    |> expectEqual
+                        (dict
+                            |> Dict.toList
+                            |> List.filter (\( k, v ) -> f k v)
+                            |> Dict.fromList
+                        )
+        , respectsInvariantsFuzz filteredFuzzer
+        ]
+
+
+partitionTest : Test
+partitionTest =
+    let
+        f : Key -> Value -> Bool
+        f _ v =
+            modBy 2 v == 0
+
+        partitionedFuzzer : Fuzzer ( Dict Key Value, Dict Key Value )
+        partitionedFuzzer =
+            Fuzz.map (Dict.partition f) dictFuzzer
+    in
+    describe "partition"
+        [ fuzz dictFuzzer "Is equivalent to toList >> List.partition >> fromList" <|
+            \dict ->
+                let
+                    ( l, r ) =
+                        Dict.partition f dict
+
+                    ( el, er ) =
+                        dict
+                            |> Dict.toList
+                            |> List.partition (\( k, v ) -> f k v)
+                            |> Tuple.mapBoth Dict.fromList Dict.fromList
+                in
+                Expect.all
+                    [ \_ -> expectEqual el l
+                    , \_ -> expectEqual er r
+                    ]
+                    ()
+        , describe "first" [ respectsInvariantsFuzz (Fuzz.map Tuple.first partitionedFuzzer) ]
+        , describe "second" [ respectsInvariantsFuzz (Fuzz.map Tuple.second partitionedFuzzer) ]
         ]
 
 
